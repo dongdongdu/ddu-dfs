3f0d611055f0a82afa77c2d9d5a690fe5e5a3220
杜栋栋 <du.dongdong@outlook.com>
2014-07-01 09:27:00 +0800

    add util.log

---
+++ test/unit/misc.java
@@ -0,0 +1,22 @@
+package unit;
+
+import static java.lang.System.out;
+import naming.PathNode;
+
+import common.Path;
+
+public class misc {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Path path = new Path();
+        out.println(path.toString());
+        // get /
+
+        PathNode pathNode = new PathNode();
+        out.println(pathNode.toString());
+        // get naming.PathNode@7f5f5897
+
+    }
+
+}
